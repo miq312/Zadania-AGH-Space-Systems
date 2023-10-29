4401f4b4b4928a2d9cc1a0fd09208d8dbf541ba1
Michał Łazarz <118307289+miq312@users.noreply.github.com>
2023-10-29 11:35:09 +0100

    Update uart.h

--- zadanie 1/Core/Inc/uart.h
+++ zadanie 1/Core/Inc/uart.h
@@ -3,10 +3,27 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-
+//// Inicjalizuje modul UART
 void UART_Init(void);
+
+// Obsluguje przychodzaca ramke danych o okreslonej dlugosci
+// Parametry:
+// len - dlugosc danych w ramce
+// payload - wskaznik do danych w ramce
 void callback(uint8_t len, uint8_t* payload);
+
+// Wysyla ramke danych za posrednictwem UART
+// Parametry:
+// id - identyfikator ramki
+// length - długosc danych w ramce
+// payload - wskaznik do danych w ramce
 void send_frame(uint8_t id, uint8_t length, uint8_t *payload);
+
+// Odbiera ramke danych za posrednictwem UART
+// Parametry:
+// id - wskaznik do zmiennej, w ktorej zostanie zapisany identyfikator ramki
+// length - wskaznik do zmiennej, w ktorej zostanie zapisana dlugosc danych w ramce
+// payload - wskaznik do danych w ramce
 void receive_frame(uint8_t *id, uint8_t *length, uint8_t *payload);
 
 
